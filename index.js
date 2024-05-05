326120720aed8354bef60b482c49dfd5281dd2d2
3obby <rfusseryiii+gh@gmail.com>
2024-05-04 22:22:59 -0400

    init, edited imports

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-import { FunctionChain, fetchCryptoPrice, openApp } from "ai-function-chain";
+import { FunctionChain } from "ai-function-chain"
+import { execute as fetchCryptoPrice } from "ai-function-chain/openAIFunctions/finance/fetchCryptoPrice.js"
+import { execute as openApp } from "ai-function-chain/openAIFunctions/unix/openApp.js"
 
 const functionChain = new FunctionChain({
   functions: [openApp, fetchCryptoPrice],
-});
+})
 
-const res1 = await functionChain.call("Open the calculator on my computer");
-const res2 = await functionChain.call("Get me the latest price of Bitcoin");
-const res3 = await functionChain.call("Get me the latest price of Ethereum");
+const res1 = await functionChain.call("Open the calculator on my computer")
+const res2 = await functionChain.call("Get me the latest price of Bitcoin")
+const res3 = await functionChain.call("Get me the latest price of Ethereum")
 
-console.log(`${res1} \n${res2} \n${res3}`);
-
+console.log(`${res1} \n${res2} \n${res3}`)
